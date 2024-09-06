a820649d4f1f1fc5cb2cfe48e442a627c08ffe80
vidhya131 <{email}>
2024-09-06 20:51:47 +0530

    version-1: pushing flask app with crud operations on in-memory list of books

--- flask-project/flask_project/app.py
+++ flask-project/flask_project/app.py
@@ -1,9 +1,33 @@
 """ Sample Flask application """
-from flask import Flask
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def get_all_books():
-    """ function to get all the books"""
-    return "hello world"
+global_books = {}
+
+@app.route("/books", methods=["POST", "GET"])
+def books():
+    """ function to get and post books"""
+
+    if request.method == 'GET':
+        return jsonify(global_books), 200
+    
+    if request.method == 'POST':
+        request_body = request.json
+        global_books[request_body['id']] = request_body
+        return jsonify(global_books), 200
+
+
+@app.route("/books/<string:id>", methods=["GET"])
+def get_book(id):
+    """ view function to get the book with given id """
+    if id in global_books:
+        return jsonify(global_books[id]), 201
+    else:
+        return "document not found", 404
+
+
+if __name__ == "__main__" :
+    app.run()
+
